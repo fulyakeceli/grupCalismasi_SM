54c5ff566192088269277a101bfe996c4fb8f4be
fulya <fnk1706@gmail.com>
2025-06-06 12:00:42 +0300

    v1.1

---
+++ src/stringManipulations/yeniDosya.java
@@ -0,0 +1,13 @@
+package stringManipulations;
+
+public class yeniDosya {
+
+    public static void main(String[] args) {
+
+        String str = "Java candir,selenium heyecan";
+
+        System.out.println(str.indexOf("candir")); // 5
+
+        System.out.println(str.indexOf('a')); //1
+    }
+}
